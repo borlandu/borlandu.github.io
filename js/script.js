5f37e99a1c5fba14a72733ed62bbfe3d8251ee45
borlandu <med36a7@mail.ru>
2017-12-11 17:05:57 +0300

    v1u4

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/*MCEXP*/
-let getTotalExp = currentLevel => currentLevel >= 0 && currentLevel < 16 ? Math.pow(currentLevel,2)+6*currentLevel :
-  currentLevel >= 16 && currentLevel < 32 ? 2.5*Math.pow(currentLevel,2)-40.5*currentLevel+360 :
-  currentLevel >= 32 ? 4.5*Math.pow(currentLevel,2)-162.5*currentLevel+2220 : 0;
-
-let getFromToExp = (fromLevel, toLevel) => getTotalExp(toLevel) - getTotalExp(fromLevel);
-
-function printTotalExp(){
-  document.querySelector('#js-totalExp').innerHTML = getTotalExp(document.querySelector('#js-currentLevel').value);
-}
-function printFromToExp(){
-  document.querySelector('#js-requiredExp').innerHTML = getFromToExp(document.querySelector('#js-fromLevel').value, document.querySelector('#js-toLevel').value)
-}
-
 /*NAV SHOW-HIDE*/
 document.querySelector('.nav-menu').onclick = _ => {
   let nav = document.querySelector('.nav');
