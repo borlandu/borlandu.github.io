534315e40347c1af0be2aaad7e75eaa67d6d14f6
borlandu <borlandu@users.noreply.github.com>
2019-06-17 08:11:52 +0300

    v1u7

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ currentLevel >= 32 ? 4.5*Math.pow(currentLevel,2)-162.5*currentLevel+2220 : 0;
 let getFromToExp = (fromLevel, toLevel) => getTotalExp(toLevel) - getTotalExp(fromLevel);
 
 function printTotalExp(){
-document.querySelector('#js-totalExp').innerHTML = getTotalExp(document.querySelector('#js-currentLevel').value);
+document.querySelector('#js-totalExp').value = getTotalExp(document.querySelector('#js-currentLevel').value);
 }
 function printFromToExp(){
-document.querySelector('#js-requiredExp').innerHTML = getFromToExp(document.querySelector('#js-fromLevel').value, document.querySelector('#js-toLevel').value)
+document.querySelector('#js-requiredExp').value = getFromToExp(document.querySelector('#js-fromLevel').value, document.querySelector('#js-toLevel').value)
 }
